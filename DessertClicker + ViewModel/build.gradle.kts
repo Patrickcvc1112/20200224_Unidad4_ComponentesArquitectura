f9b805a0f73cb2db7df348d730328a140373b24c
Patrickcvc1112 <patrickcvc1112@gmail.com>
2023-09-30 11:54:48 -0500

    v1

---
+++ DessertClicker + ViewModel/build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript {
+    ext {
+        var lifecycle_version = "2.5.1"
+    }
+}
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
